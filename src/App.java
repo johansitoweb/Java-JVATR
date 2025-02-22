3f46697c2d9f968c26c28abd2a733f256280759c
Johan Antonio <147890607+johansitoweb@users.noreply.github.com>
2025-02-22 10:57:03 -0400

    JAVA

---
+++ src/App.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Esto es una salida de dato o informacion");
+        // comentario de una linea
+        /*
+         * comentario
+         * de varias
+         * lineas
+         */
+        // variable
+        String name;
+        name = "johansitoweb";
+        System.out.println(name);
+
+        // tipos de datos
+        int EnteroNumero;
+        EnteroNumero = 30;
+        float decimalNumero = 34.43f;
+        char caracterLetra = 'c';
+        System.out.println(caracterLetra);
+        System.out.println("Este numero es un entero " + EnteroNumero);
+        System.out.println("Este numero es decimal" + decimalNumero);
+        System.out.println("Este es un catacter" + caracterLetra);
+    }
+}
